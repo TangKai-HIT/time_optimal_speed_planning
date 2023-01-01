becb323ded9f145c376fe7421f54647de02061db
TangKai-HIT <1292737216@qq.com>
2023-01-01 23:31:11 +0800

    test passed

--- curves/getCubicSpline_wp.m
+++ curves/getCubicSpline_wp.m
@@ -1,11 +1,12 @@
-function splineParams=getCubicSpline_wp(q_wp, t_wp, init_vel, end_vel)
+function [splineCoeff, vel]=getCubicSpline_wp(q_wp, t_wp, init_vel, end_vel)
 % GETCUBICSPLINE_WP get cubic spline parameters for way point interpolation with specified initial and final velocities, 
 % intermediate way point velocities are computed by assuming continuous acceleration.
 %   q_wp: waypoints, n X dim
 %   t_wp: time at waypoints 1 X n
 %   init_vel: 1 X dim
 %   end_vel: 1 X dim
-%   splineParams: parameter struct
+%   splineCoeff: 1 X 4 cell for spline coefficients
+%   vel: velocities on way points
 
 dim = size(q_wp, 2);
 N = length(t_wp);
@@ -14,7 +15,7 @@ T = diff(t_wp);
 %% Solve intermediate velocities
 tridiag_v = zeros(N-2, 3); %store tridiagonal elements of the sparse tridiagonal matrix(N-2 X N-2)
 c = zeros(N-2, dim); 
-
+%init parameters
 c(1, :) = -T(2)*init_vel;
 c(end, :) = -T(end-1)*end_vel;
 
@@ -30,5 +31,19 @@ for n=1:N-2
 
     c(n, :) = c(n, :) + 3/(T(n)*T(n+1)) * (T(n)^2 * (q_wp(n+2,:) - q_wp(n+1,:)) + T(n+1)^2 * (q_wp(n+1,:) - q_wp(n,:)));
 end
+% solve linear equation with tridiagonal matrix
+v = solveTridiag(tridiag_v, c);
+% return vel
+vel  = zeros(N, dim);
+vel(1, :) = init_vel;
+vel(end, :) = end_vel;
+vel(2:end-1, :) = v;
 
-
+%% Compute spline coefficients
+splineCoeff = {zeros(N-1, dim), zeros(N-1, dim), zeros(N-1, dim), zeros(N-1, dim)};
+for k=1:N-1
+    splineCoeff{1}(k, :) = q_wp(k, :);
+    splineCoeff{2}(k, :) = vel(k, :);
+    splineCoeff{3}(k, :) = (3*(q_wp(k+1, :) - q_wp(k, :))/T(k) - 2*vel(k, :) - vel(k+1, :)) / T(k);
+    splineCoeff{4}(k, :) = (2*(q_wp(k, :) - q_wp(k+1, :))/T(k) + vel(k, :) + vel(k+1, :)) / (T(k)^2);
+end
