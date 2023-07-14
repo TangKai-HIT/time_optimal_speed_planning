ea23c79f8ab7c91661d6afc79ed0a4a2b6d6825b
TangKai-HIT <1292737216@qq.com>
2023-07-14 09:46:35 +0800

    upated

--- speed_planning/getSpeedPlanConstraints.m
+++ speed_planning/getSpeedPlanConstraints.m
@@ -4,11 +4,11 @@ function Constraints = getSpeedPlanConstraints(Phi, Alpha, Mu, v_init, v_end)
 %       Phi: 1 X dim -- speed constraints
 %       Alpha: 1 X dim -- acceleration constraints
 %       Mu: 1 X dim -- torque constraints
-%       v_init: scalar -- initial speed
-%       v_end: scalar -- end speed
+%       v_init: 1 X dim -- initial speed
+%       v_end: 1 X dim -- end speed
 
 Constraints.Phi = Phi;
 Constraints.Alpha = Alpha;
 Constraints.Mu = Mu;
-Constraints.b_init = v_init^2;
-Constraints.b_end = v_end^2;
+Constraints.b_init = v_init.^2;
+Constraints.b_end = v_end.^2;
